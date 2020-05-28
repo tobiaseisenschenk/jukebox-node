15d1673c179758b466d4a6d105b050f83428d399
Tobi <tobias.eisenschenk@nui.care>
2020-05-28 19:34:53 +0200

    feat: serve generated audio and lint project

--- jukebox.js
+++ jukebox.js
@@ -1,12 +1,20 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.json());
+const express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-app.post('/', function (req, res) {
-  console.log('logged: ', req.body);
-  res.status(200).send();
-})
+
+const app = express()
+
+app.set('view engine', 'ejs');
+// eslint-disable-next-line no-undef
+app.set('views', __dirname + '/views');
+app.use(bodyParser.json());
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
  
 app.listen(3023);
 console.info('listening on port 3023');
